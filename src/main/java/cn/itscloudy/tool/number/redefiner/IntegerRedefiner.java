79d4095466c77e6e33d70dc74b2800df5288a17a
chaijiaqi <itscloudy@foxmail.com>
2021-07-05 18:05:40 +0800

    Chai [N/A] test: add tests for NumberConverter & IntegerRedefiner

--- src/main/java/cn/itscloudy/tool/number/redefiner/IntegerRedefiner.java
+++ src/main/java/cn/itscloudy/tool/number/redefiner/IntegerRedefiner.java
@@ -2,10 +2,12 @@ package cn.itscloudy.tool.number.redefiner;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiFunction;
 
 public abstract class IntegerRedefiner {
 
     private final char[] chars;
+    private final char negative;
     private final Map<Character, Integer> charIntMap;
     private final int radix;
 
@@ -16,6 +18,7 @@ public abstract class IntegerRedefiner {
             throw new NumberFormatException("radix " + radix +
                     " less than Character.MIN_RADIX");
         }
+        this.negative = negative();
 
         this.charIntMap = new HashMap<>();
         for (int i = 0; i < chars.length; i++) {
@@ -29,46 +32,57 @@ public abstract class IntegerRedefiner {
 
     abstract char[] chars();
 
+    public char negative() {
+        return '-';
+    }
+
     public String add(String a, String b) {
-        int intA = stringToInt(a);
-        int intB = stringToInt(b);
-        return intToString(intA + intB);
+        return operate(a, b, Integer::sum);
     }
 
     public String minus(String a, String b) {
-        int intA = stringToInt(a);
-        int intB = stringToInt(b);
-        return intToString(intA - intB);
+        return operate(a, b, (iA, iB) -> iA - iB);
     }
 
     public String multiply(String a, String b) {
-        int intA = stringToInt(a);
-        int intB = stringToInt(b);
-        return intToString(intA * intB);
+        return operate(a, b, (iA, iB) -> iA * iB);
     }
 
     public String divide(String a, String b) {
-        int intA = stringToInt(a);
-        int intB = stringToInt(b);
-        return intToString(intA / intB);
+        return operate(a, b, (iA, iB) -> iA / iB);
     }
 
     public String mod(String a, String b) {
+        return operate(a, b, (iA, iB) -> iA % iB);
+    }
+
+    private String operate(String a, String b, BiFunction<Integer, Integer, Integer> intOperation) {
+        if (empty(a) || empty(b)) {
+            throw new NumberFormatException("A and B must not be empty");
+        }
         int intA = stringToInt(a);
         int intB = stringToInt(b);
-        return intToString(intA % intB);
+
+        Integer opResult = intOperation.apply(intA, intB);
+        return intToString(opResult);
+    }
+
+    private boolean empty(String s) {
+        return null == s || "".equals(s);
     }
 
     private int stringToInt(String s) {
         int result = 0;
         int base = 1;
         char[] chars = s.toCharArray();
-        for (int i = chars.length - 1; i >= 0; i--) {
+        boolean negative = chars[0] == this.negative;
+        int firstIndex = negative ? 1 : 0;
+        for (int i = chars.length - 1; i >= firstIndex; i--) {
             char c = chars[i];
             result += base * charToInt(c);
             base *= radix;
         }
-        return result;
+        return negative ? -result : result;
     }
 
     private Integer charToInt(char c) {
@@ -80,6 +94,12 @@ public abstract class IntegerRedefiner {
     }
 
     private String intToString(int i) {
+        if (i == 0) {
+            return chars[0] + "";
+        }
+        boolean positive = i > 0;
+        i = Math.abs(i);
+
         StringBuilder sb = new StringBuilder();
         while (i > 0) {
             int mod = i % radix;
@@ -87,6 +107,6 @@ public abstract class IntegerRedefiner {
             sb.insert(0, s);
             i = i / radix;
         }
-        return sb.toString();
+        return positive ? sb.toString() : sb.insert(0, '-').toString();
     }
 }
---
+++ src/test/java/cn/itscloudy/tool/number/redefiner/IntegerRedefinerTest.java
@@ -0,0 +1,90 @@
+package cn.itscloudy.tool.number.redefiner;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.function.BiFunction;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class IntegerRedefinerTest extends IntegerRedefiner {
+
+    @Override
+    protected char[] chars() {
+        return new char[]{'@', '#', '$'};
+    }
+
+    @ParameterizedTest(name = "{0} add {1}")
+    @CsvSource({
+            ", #, EX",
+            "@@, @@@@, @",
+            "#, #, $",
+            "##, #, #$"
+    })
+    public void should_add(String a, String b, String expectation) {
+        assertEquals(a, b, expectation, this::add);
+    }
+
+    @ParameterizedTest(name = "{0} minus {1}")
+    @CsvSource({
+            ", #, EX",
+            "$, $, @",
+            "$$, $, $@",
+            "$, ##, -$"
+    })
+    public void should_minus(String a, String b, String expectation) {
+        assertEquals(a, b, expectation, this::minus);
+    }
+
+
+    @ParameterizedTest(name = "{0} multiply {1}")
+    @CsvSource({
+            ", #, EX",
+            "@@, @@, @",
+            "#, @, @",
+            "@, #, @",
+            "#, #, #",
+            "##, $$, #@#$",
+            "##, -$$, -#@#$",
+    })
+    public void should_multiply(String a, String b, String expectation) {
+        assertEquals(a, b, expectation, this::multiply);
+    }
+
+
+    @ParameterizedTest(name = "{0} divide {1}")
+    @CsvSource({
+            ", #, EX",
+            "@, @, EX",
+            "#, #, #",
+            "#, $, @",
+            "#@#$, $$, ##",
+            "-#@#$, $$, -##"
+    })
+    public void should_divide(String a, String b, String expectation) {
+        assertEquals(a, b, expectation, this::divide);
+    }
+
+    @ParameterizedTest(name = "{0} divide {1}")
+    @CsvSource({
+            ", #, EX",
+            "#@, $, #",
+            "##, $, @",
+            "$$, -#@, $"
+    })
+    public void should_mod(String a, String b, String expectation) {
+        assertEquals(a, b, expectation, this::mod);
+    }
+
+
+    public void assertEquals(String a, String b, String expectation,
+                             BiFunction<String, String, String> fun) {
+        if ("EX".equals(expectation)) {
+            assertThrows(Exception.class, () -> fun.apply(a, b));
+            return;
+        }
+        Assertions.assertEquals(expectation, fun.apply(a, b));
+    }
+
+}
