1d91dc47fdca56d2d4b66545bfc5153d64effd06
chaijiaqi <itscloudy@foxmail.com>
2021-07-20 20:24:51 +0800

    Chai [N/A] refactor: OptionalResultTest

--- src/test/java/cn/itscloudy/snippedjava/tool/other/OptionalResultTest.java
+++ src/test/java/cn/itscloudy/snippedjava/tool/other/OptionalResultTest.java
@@ -2,6 +2,8 @@ package cn.itscloudy.snippedjava.tool.other;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class OptionalResultTest {
@@ -10,24 +12,33 @@ public class OptionalResultTest {
     public void shouldContainValue() {
         String result = "MONKEY";
         OptionalResult<String> or = OptionalResult.of(() -> result);
+
         assertTrue(or.isSuccess());
         assertEquals(result, or.get());
         assertNull(or.exception());
         assertEquals(result, or.orElseThrow(e -> new RuntimeException("E")));
+
+        Optional<String> optional = or.optional();
+        assertTrue(optional.isPresent());
+        assertEquals(result, optional.get());
     }
 
     @Test
     public void shouldContainException() {
+        RuntimeException ex = new RuntimeException("Runtime Exception");
         OptionalResult<String> or = OptionalResult.of(() -> {
             if (System.currentTimeMillis() > 0) {
-                throw new RuntimeException("Runtime Exception");
+                throw ex;
             }
             return "Unexpected Value";
         });
 
         assertFalse(or.isSuccess());
         assertNull(or.get());
-        assertNotNull(or.exception());
+        assertEquals(ex, or.exception());
         assertThrows(RuntimeException.class, () -> or.orElseThrow(e -> new RuntimeException("E")));
+
+        Optional<String> optional = or.optional();
+        assertTrue(optional.isEmpty());
     }
 }
